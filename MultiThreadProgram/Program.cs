12b7ef29e222268a98dab22ead7e6217e8542d02
zj_yuan <zj_yuan@163.com>
2021-03-23 16:11:49 +0800

    chapter2 线程同步(下)

--- MultiThreadProgram/Program.cs
+++ MultiThreadProgram/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 
 namespace MultiThreadProgram
@@ -14,7 +15,7 @@ namespace MultiThreadProgram
             }
         }
         static SemaphoreSlim _semaphore = new SemaphoreSlim(4);
-        static void AccessDataBase(string name,int seconds)
+        static void AccessDataBase(string name, int seconds)
         {
             Console.WriteLine($"{name} waite to access a database");
             _semaphore.Wait();
@@ -28,32 +29,207 @@ namespace MultiThreadProgram
         private static AutoResetEvent _mainEvent = new AutoResetEvent(false);
         static void Process(int seconds)
         {
-            Console.WriteLine("Starting a long running work...");
+            Console.WriteLine("Starting a long running work...");//2 
             Thread.Sleep(TimeSpan.FromSeconds(seconds));
-            Console.WriteLine("Work is done!");
+            Console.WriteLine("Work is done!");// 3
             _workerEvent.Set();
-            Console.WriteLine("Waiting for a main thread to complete its work");
+            Console.WriteLine("Waiting for a main thread to complete its work");//4
             _mainEvent.WaitOne();
-            Console.WriteLine("Starting second operation...");
+            Console.WriteLine("Starting second operation...");//7
             Thread.Sleep(TimeSpan.FromSeconds(seconds));
-            Console.WriteLine("Work is done!");
+            Console.WriteLine("Work is done!");//9
             _workerEvent.Set();
+        }
+        static void TraveThroughGates(string threadName, int seconds)
+        {
+            Console.WriteLine($"{threadName} fails to sleep");
+            Thread.Sleep(TimeSpan.FromSeconds(seconds));
+            Console.WriteLine($"{threadName} waits for the gates to open!");
+            _mainEvent2.Wait();
+            Console.WriteLine($"{threadName} enters the gates");
+        }
+        static ManualResetEventSlim _mainEvent2 = new ManualResetEventSlim();
+
+        static CountdownEvent _countdown = new CountdownEvent(2);
+        static void PerformOperation(string message, int seconds)
+        {
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(seconds));
+                Console.WriteLine(message);
+                _countdown.Signal();
+            }
+        }
+        static Barrier _barrier = new Barrier(2, (b) => Console.WriteLine($"End of phase {b.CurrentPhaseNumber + 1}"));
+        static void PlayMusic(string name, string message, int seconds)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("------------------------------");
+                Thread.Sleep(TimeSpan.FromSeconds(seconds));
+                Console.WriteLine($"{name} starts to {message}");
+                Thread.Sleep(TimeSpan.FromSeconds(seconds));
+                Console.WriteLine($"{name} finishes to {message}");
+                _barrier.SignalAndWait();
+            }
+        }
+        static ReaderWriterLockSlim _rw = new ReaderWriterLockSlim();
+        static Dictionary<int, int> _dict_rw = new Dictionary<int, int>();
+        static void Read()
+        {
+            Console.WriteLine("Reading contents of a dictionary");
+            while (true)
+            {
+
+                try
+                {
+                    _rw.EnterReadLock();
+                    foreach (var item in _dict_rw)
+                    {
+                        Thread.Sleep(TimeSpan.FromSeconds(0.1));
+                    }
+                }
+                finally
+                {
+                    _rw.ExitReadLock();
+                }
+            }
+        }
+        static void Write(string threadName)
+        {
+            Console.WriteLine("Writing contents of a dictionary");
+            while (true)
+            {
+                try
+                {
+                    int newKey = new Random().Next(1000);
+                    _rw.EnterUpgradeableReadLock();
+                    if (!_dict_rw.ContainsKey(newKey))
+                    {
+                        try
+                        {
+                            _rw.EnterWriteLock();
+                            _dict_rw[newKey] = 1;
+                            Console.WriteLine($"New Key {newKey} is added to a dictionary by a {threadName}");
+                            Thread.Sleep(TimeSpan.FromSeconds(0.1));
+                        }
+                        finally
+                        {
+                            _rw.ExitWriteLock();
+                        }
+                    }
+                }
+                finally
+                {
+                    _rw.ExitUpgradeableReadLock();
+                }
+            }
+
+        }
+        static volatile bool _isCompleted = false;
+        static void UserModeWait()
+        {
+            while (!_isCompleted)
+            {
+                Console.WriteLine(".");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Waiting is complete");
+        }
+        static void HybridSpinWait()
+        {
+            var w = new SpinWait();
+            while (!_isCompleted)
+            {
+                w.SpinOnce();
+                Console.WriteLine(w.NextSpinWillYield);
+            }
+            Console.WriteLine("Waiting is complete");
         }
         static void Main(string[] args)
         {
             {
+                var t1 = new Thread(UserModeWait);
+                var t2 = new Thread(HybridSpinWait);
+                Console.WriteLine("Running user mode waiting");
+                t1.Start();
+                //Thread.Sleep(TimeSpan.FromSeconds(20));
+                Thread.Sleep(20);
+                _isCompleted = true;
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+                _isCompleted = false;
+                Console.WriteLine("Running hybrid SpinWait construct waiting"); ;
+                t2.Start();
+                Thread.Sleep(TimeSpan.FromSeconds(50));
+                //Thread.Sleep(5);
+                _isCompleted = true;
+            }
+            return;
+            {
+                new Thread(Read) { IsBackground = true }.Start();
+                new Thread(Read) { IsBackground = true }.Start();
+                new Thread(Read) { IsBackground = true }.Start();
+                new Thread(() => Write("Thread 1")) { IsBackground = true }.Start();
+                new Thread(() => Write("Thread 2")) { IsBackground = true }.Start();
+                Thread.Sleep(TimeSpan.FromSeconds(30));
+            }
+            return;
+            {
+                // 2.8 
+                var t1 = new Thread(() => PlayMusic("the guitarist", "paly an amazing solo", 5));
+                var t2 = new Thread(() => PlayMusic("the singer", "sing his song", 2));
+                var t3 = new Thread(() => PlayMusic("the painter", "drwa the paper", 18));
+                t1.Start();
+                t2.Start();
+                t3.Start();
+            }
+            return;
+            {
+                // 2.7 使用 CountDownEvent
+                Console.WriteLine("Satarting two operations");
+                var t1 = new Thread(() => PerformOperation("Operation 1 is completed", 4));
+                var t2 = new Thread(() => PerformOperation("Operation 2 is completed", 5));
+                t1.Start();
+                t2.Start();
+                _countdown.Wait();
+                Console.WriteLine("Both operations have been completed");
+                _countdown.Dispose();
+            }
+            return;
+            {
+                // 2.6 使用manualResetEventSlim
+                var t1 = new Thread(() => TraveThroughGates("Thread 1", 5));
+                var t2 = new Thread(() => TraveThroughGates("Thread 2", 6));
+                var t3 = new Thread(() => TraveThroughGates("Thread 3", 12));
+                t1.Start();
+                t2.Start();
+                t3.Start();
+                Thread.Sleep(TimeSpan.FromSeconds(6));
+                Console.WriteLine("The gates are now open");
+                _mainEvent2.Set();
+                Thread.Sleep(TimeSpan.FromSeconds(2));
+                _mainEvent2.Reset();
+                Console.WriteLine("The gates have been closed");
+                Thread.Sleep(TimeSpan.FromSeconds(10));
+                Console.WriteLine("The gates are now open for the second time!");
+                _mainEvent2.Set();
+                Thread.Sleep(TimeSpan.FromSeconds(2));
+                Console.WriteLine("The gates have been closed!");
+                _mainEvent2.Reset();
+            }
+            return;
+            {
                 // 2.5 使用AutoResetEvent
                 var t = new Thread(() => Process(10));
                 t.Start();
-                Console.WriteLine("Waiting for another thread to complete work");
+                Console.WriteLine("Waiting for another thread to complete work");//1
                 _workerEvent.WaitOne();
-                Console.WriteLine("First operation is completed!");
-                Console.WriteLine("Performing an operation on a main thread");
+                Console.WriteLine("First operation is completed!");//5
+                Console.WriteLine("Performing an operation on a main thread");//6
                 Thread.Sleep(TimeSpan.FromSeconds(5));
                 _mainEvent.Set();
-                Console.WriteLine("Now running the second operation on a second thread");
+                Console.WriteLine("Now running the second operation on a second thread");//8
                 _workerEvent.WaitOne();
-                Console.WriteLine("Second operation is completed!");
+                Console.WriteLine("Second operation is completed!");//10
             }
             return;
             {
